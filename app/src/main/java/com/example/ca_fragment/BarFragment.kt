95cd299103bf8720fbced07d4bb72a015cc7868a
kitkatie2823 <kitkatLT2823@gmail.com>
2025-02-27 16:00:24 -0800

    broken bar frag

--- app/src/main/java/com/example/ca_fragment/BarFragment.kt
+++ app/src/main/java/com/example/ca_fragment/BarFragment.kt
@@ -1,40 +1,72 @@
 package com.example.ca_fragment
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.androidplot.xy.BarFormatter
-import com.androidplot.xy.SimpleXYSeries
-import com.androidplot.xy.XYPlot
-import com.androidplot.xy.XYSeries
-import com.androidplot.xy.XYGraphWidget
-import com.androidplot.xy.BoundaryMode
-import java.util.Arrays
+import com.androidplot.xy.*
 
 class BarFragment : Fragment() {
+
+    private lateinit var plot: XYPlot
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.bar_fragment, container, false)
+    ): View {
+        val rootView = inflater.inflate(R.layout.bar_fragment, container, false)
+        plot = rootView.findViewById(R.id.plot) // Find the plot in XML
+        return rootView
+    }
 
-        // Find XYPlot in the layout
-        val plot = view.findViewById<XYPlot>(R.id.plot)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        // Create data series
-        val series = SimpleXYSeries(
-            Arrays.asList(1, 2, 3, 4),
-            Arrays.asList(4.3, 2.5, 3.5, 4.5),
-            "Bar Data"
-        )
+        if (::plot.isInitialized) {
+            val xValues = listOf(1, 2, 3, 4)
+            val xOffset = 0.2  // Small offset for side-by-side effect
 
-        // Format and add series
-        val formatter = BarFormatter()
-        plot.addSeries(series, formatter)
+            // A Bars (Left)
+            val seriesA: XYSeries = SimpleXYSeries(
+                xValues.map { it - xOffset }, // Shifted left
+                listOf(4.3, 2.5, 3.5, 4.5),
+                "A"
+            )
+            val formatA = BarFormatter(Color.RED, Color.BLACK)
 
-        return view
+            // B Bars (Center)
+            val seriesB: XYSeries = SimpleXYSeries(
+                xValues, // No shift
+                listOf(2.4, 4.4, 1.8, 2.8),
+                "B"
+            )
+            val formatB = BarFormatter(Color.GREEN, Color.BLACK)
+
+            // C Bars (Right)
+            val seriesC: XYSeries = SimpleXYSeries(
+                xValues.map { it + xOffset }, // Shifted right
+                listOf(2, 2, 3, 5),
+                "C"
+            )
+            val formatC = BarFormatter(Color.BLUE, Color.BLACK)
+
+            // Add series to plot
+            plot.addSeries(seriesA, formatA)
+            plot.addSeries(seriesB, formatB)
+            plot.addSeries(seriesC, formatC)
+
+            // Adjust bar width to fit properly
+            plot.getRenderer(BarRenderer::class.java)?.setBarGroupWidth(
+                BarRenderer.BarGroupWidthMode.FIXED_WIDTH, 60f
+            )
+
+            // Redraw to apply changes
+            plot.redraw()
+        } else {
+            Log.e("BarFragment", "plot is not initialized!")
+        }
     }
 }
